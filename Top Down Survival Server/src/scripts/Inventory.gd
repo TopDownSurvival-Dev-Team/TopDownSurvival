fe30b2c4633f81fbc28b3cc32c56c22104376600
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-10-31 21:47:15 +0400

    fixed small bug

--- Top Down Survival Server/src/scripts/Inventory.gd
+++ Top Down Survival Server/src/scripts/Inventory.gd
@@ -15,7 +15,7 @@ func add_item_s(player_id: int, item_id: String, quantity: int):
 	
 	if current_quantity:
 		current_quantity += quantity
-		Database.set_item_quantity(player_uid, item_id, quantity)
+		Database.set_item_quantity(player_uid, item_id, current_quantity)
 		rpc_id(player_id, "update_item", item_id, current_quantity)
 	else:
 		Database.create_new_item(player_uid, item_id, quantity)
