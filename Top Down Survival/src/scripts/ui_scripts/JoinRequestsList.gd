0fa5003c7aff87c4730c15971288b28034d61d57
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-12-30 02:16:17 +0400

    in game join request alerts

---
+++ Top Down Survival/src/scripts/ui_scripts/JoinRequestsList.gd
@@ -0,0 +1,19 @@
+extends Control
+
+const JOIN_REQUEST_SCENE = preload("res://src/scenes/ui_scenes/templates/JoinRequest.tscn")
+
+
+func _ready():
+    # Remove preview nodes
+    for node in get_children():
+        remove_child(node)
+        node.queue_free()
+
+    GodotcordActivityManager.connect("activity_join_request", self, "new_join_request")
+
+
+func new_join_request(username: String, user_id: int):
+    print("New join request from %s" % username)
+    var join_request = JOIN_REQUEST_SCENE.instance()
+    join_request.init(username, user_id)
+    add_child(join_request)
---
+++ Top Down Survival/src/scripts/ui_scripts/templates/JoinRequest.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var username: String
+var user_id: int
+
+onready var label = $H/Label
+
+
+func init(_username: String, _user_id: int):
+    username = _username
+    user_id = _user_id
+    name = str(_user_id)
+
+
+func _ready():
+    label.set_text("%s wants to join your game" % username)
+
+
+func _on_Accept_pressed():
+    GodotcordActivityManager.send_request_reply(user_id, GodotcordActivity.YES)
+    get_parent().remove_child(self)
+    queue_free()
+
+
+func _on_Decline_pressed():
+    GodotcordActivityManager.send_request_reply(user_id, GodotcordActivity.NO)
+    get_parent().remove_child(self)
+    queue_free()
