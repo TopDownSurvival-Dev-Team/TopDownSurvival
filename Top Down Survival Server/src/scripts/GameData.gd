bbad0eca481e349e5fcc22c6aafe6e5c16cc0469
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-01-07 20:27:25 +0400

    loading recipe_data

--- Top Down Survival Server/src/scripts/GameData.gd
+++ Top Down Survival Server/src/scripts/GameData.gd
@@ -1,22 +1,44 @@
 extends Node
 
 const DATA_PATH = "res://data"
+const ITEM_DATA_PATH = "%s/item_data.json" % DATA_PATH
+const RECIPE_DATA_PATH = "%s/recipe_data" % DATA_PATH
 const PLAYER_REACH = 128
 
 var item_data: Dictionary
+var recipe_data: Array
 
 
 func _ready():
-    # Read item data
+    var dir = Directory.new()
     var file = File.new()
-    var item_data_path = "%s/item_data.json" % DATA_PATH
-    file.open(item_data_path, File.READ)
+
+    # Item data
+    file.open(ITEM_DATA_PATH, File.READ)
     item_data = parse_json(file.get_as_text())
     file.close()
+
+    # Recipe data
+    dir.open(RECIPE_DATA_PATH)
+    dir.list_dir_begin(true, true)
+    var file_name = dir.get_next()
+
+    while file_name != "":
+        if not dir.current_is_dir():
+            file.open("%s/%s" % [RECIPE_DATA_PATH, file_name], File.READ)
+            recipe_data.resize(len(recipe_data) + 1)
+            var recipe_level = file_name.replace("level_", "").replace(".json", "").to_int()
+            var recipes = parse_json(file.get_as_text())
+            recipe_data[recipe_level] = recipes
+            file.close()
+
+        file_name = dir.get_next()
+
+    dir.list_dir_end()
 
     # Connect signals
     Network.connect("player_joined_game", self, "send_game_data_s")
 
 
 func send_game_data_s(player_id: int):
-    rpc_id(player_id, "send_game_data", item_data, PLAYER_REACH)
+    rpc_id(player_id, "send_game_data", item_data, recipe_data, PLAYER_REACH)
--- Top Down Survival/src/autoload_scripts/GameData.gd
+++ Top Down Survival/src/autoload_scripts/GameData.gd
@@ -4,6 +4,7 @@ const VERSION_FILE = "res://version.txt"
 
 var client_version: String
 var item_data: Dictionary
+var recipe_data: Array
 var player_reach: int
 
 
@@ -14,6 +15,7 @@ func _ready():
     f.close()
 
 
-remote func send_game_data(_item_data: Dictionary, _player_reach: int):
+remote func send_game_data(_item_data: Dictionary, _recipe_data: Array, _player_reach: int):
     item_data = _item_data
+    recipe_data = _recipe_data
     player_reach = _player_reach
