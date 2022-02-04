884768e0e0a7003c441b44b01c0757b11ff37db3
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-02-04 15:17:40 +0400

    added chest item scene

--- Top Down Survival Server/src/scripts/ContainerMenu.gd
+++ Top Down Survival Server/src/scripts/ContainerMenu.gd
@@ -5,5 +5,7 @@ func show_menu_s(
 	player_id: int, container_name: String, container_position: Vector2
 ):
 	var container_id = Database.get_container_id(container_position)
-	var items = Database.get_container_items(container_id)
-	rpc_id(player_id, "show_menu", container_name, items)
+
+	if container_id:
+		var items = Database.get_container_items(container_id)
+		rpc_id(player_id, "show_menu", container_name, items)
